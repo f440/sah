4708ce2174f7be059eb16f9086495acdbdf87e0e
f440 <freq440@gmail.com>
2015-10-06 23:44:06 +0900

    Add subcommand skeleton

--- main.go
+++ main.go
@@ -9,6 +9,73 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "sah"
+	app.Usage = "Command line util for Atlassian Bitbucket Server"
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "browse",
+			Usage: "browse repository",
+			Action: func(c *cli.Context) {
+				// TODO
+			},
+		},
+		{
+			Name:  "clone",
+			Usage: "clone repository",
+			Action: func(c *cli.Context) {
+				// TODO
+			},
+		},
+		{
+			Name:  "create",
+			Usage: "create repository",
+			Action: func(c *cli.Context) {
+				// TODO
+			},
+		},
+		{
+			Name:  "fork",
+			Usage: "fork repository",
+			Action: func(c *cli.Context) {
+				// TODO
+			},
+		},
+		{
+			Name:  "project",
+			Usage: "Show project information",
+			Action: func(c *cli.Context) {
+				// TODO
+			},
+		},
+		{
+			Name:  "repository",
+			Usage: "Show repository information",
+			Action: func(c *cli.Context) {
+				// TODO
+			},
+		},
+		{
+			Name:  "upstream",
+			Usage: "Show upstream information",
+			Action: func(c *cli.Context) {
+				// TODO
+			},
+		},
+		{
+			Name:  "user",
+			Usage: "Show user information",
+			Action: func(c *cli.Context) {
+				// TODO
+			},
+		},
+		{
+			Name:  "version",
+			Usage: "Show version",
+			Action: func(c *cli.Context) {
+				// TODO
+			},
+		},
+	}
 
 	app.Run(os.Args)
 }
